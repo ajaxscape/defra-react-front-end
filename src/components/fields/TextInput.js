f63ae18f94f5c127c38998d36a4c8869aa2c517c
ben <ben@ajaxscape.com>
2020-02-17 17:18:10 +0000

    Fixed text input issue

--- src/components/fields/TextInput.js
+++ src/components/fields/TextInput.js
@@ -15,7 +15,7 @@ export default function TextInput (props) {
   const { id, name = id, value: initialValue, type = 'text', label = null, labelHidden = null, hint, className = '' } = props
   const hintId = hint ? `${id}-hint` : null
 
-  const [value, setValue] = useState(initialValue)
+  const [value, setValue] = useState(initialValue || '')
 
   function onChange (e) {
     setValue(e.target.value)
@@ -23,8 +23,6 @@ export default function TextInput (props) {
 
   const { data } = useContext(FormContext)
   data[name] = value
-
-  console.log('...data => ', data)
 
   return (
     <div className="govuk-form-group">
@@ -36,9 +34,10 @@ export default function TextInput (props) {
         name={name}
         type={type}
         aria-describedby={hintId}
+        autoComplete="on"
         spellCheck="false"
         onChange={onChange}
-        // value={value}
+        value={value}
       />
     </div>
   )
