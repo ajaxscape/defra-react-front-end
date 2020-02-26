839d1f35fcc10cb4cba30828c4528e525b9ed3c2
ben <ben@ajaxscape.com>
2020-02-26 18:07:21 +0000

    Testing with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
 import App from './App';
 
-test('Home page ok', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello world/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter() })
+
+it('Home page ok', () => {
+
 });
