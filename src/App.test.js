a92deca50780921a3b846d40858da62df941c0f2
ben <ben@ajaxscape.com>
2020-03-02 07:20:35 +0000

    Learning tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Enzyme from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
 import App from './App';
 
 Enzyme.configure({adapter: new EnzymeAdapter() })
 
-it('Home page ok', () => {
-
+test('Renders without crashing', () => {
+  const wrapper = shallow(<App/>)
 });
